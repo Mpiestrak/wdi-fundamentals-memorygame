33862d069ba7032fc4e27670f872f169fcc76068
Matt <mpiestrak@gmail.com>
2018-09-15 18:38:01 -0400

    Rough Finished

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,6 +20,15 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+var createBoard = function() {
+	for(var i=0; i<cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', [i]);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement)
+	}
+}
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -28,12 +37,20 @@ var checkForMatch = function() {
 			alert("Sorry, try again.");
 		}
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].suit);
 	console.log("User flipped " + cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
-flipCard(0)
-flipCard(2)
+}
+var reset = function() {
+	location.reload();
+}
+
+document.getElementsByTagName('button')[0].addEventListener('click', reset);
+createBoard();
